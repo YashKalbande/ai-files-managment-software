b7de21a6440a7fd6fa73acf3bc2d3bbf9e2516a8
Yash.Kalbnde <53596464+YashKalbande@users.noreply.github.com>
2020-09-30 19:04:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import os
 from ClassificationOfFile import create_dirs, arrange
 from ImageHandling import img_metadata, loop_jpg
 from ImgDatabase import bullk_insert_img_file
-from TextFileHandling import  text_metadata,loop_text
-from TextDatabase import  bullk_insert_text_file
 
 class Main:
     create_dirs()
@@ -11,7 +9,4 @@ class Main:
     current_dir = (os.getcwd())
     loop_jpg(os.path.join(current_dir + "\jpg_files"))
     bullk_insert_img_file(img_metadata)
-    os.chdir(current_dir)
-    loop_text(os.path.join(current_dir+"\docx_files"))
-    bullk_insert_text_file(text_metadata)
     print("End")
